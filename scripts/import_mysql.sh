017440e6061324bdbd90d21e4feba6ead9b423f9
damianhalgas <haxv1ct0ri0us@gmail.com>
2025-01-14 15:40:59 +0100

    repair: sh file7

--- scripts/import_mysql.sh
+++ scripts/import_mysql.sh
@@ -3,9 +3,9 @@
 # Zmienne konfiguracyjne
 CONTAINER_NAME="mysql-container"  # Nazwa kontenera MySQL
 DB_NAME="mydatabase"              # Nazwa bazy danych
-DB_ROOT_USER="root"               # Użytkownik root
-DB_ROOT_PASSWORD="rootpassword"   # Hasło użytkownika root
-CSV_SOURCE_DIR="/home/administrator/vm_db_2025/csv/20K"  # Lokalizacja plików CSV 
+DB_USER="root"                    # Użytkownik MySQL
+DB_PASSWORD="rootpassword"        # Hasło użytkownika MySQL
+CSV_SOURCE_DIR="/home/administrator/vm_db_2025/csv/20K"  # Lokalizacja plików CSV
 CSV_TARGET_DIR="/tmp"             # Lokalizacja plików CSV w kontenerze
 
 # Sprawdzenie, czy pliki CSV istnieją w źródłowej lokalizacji
@@ -22,58 +22,53 @@ docker cp "$CSV_SOURCE_DIR/dane_osobowe.csv" $CONTAINER_NAME:"$CSV_TARGET_DIR/da
 docker cp "$CSV_SOURCE_DIR/dane_kontaktowe.csv" $CONTAINER_NAME:"$CSV_TARGET_DIR/dane_kontaktowe.csv"
 docker cp "$CSV_SOURCE_DIR/dane_firmowe.csv" $CONTAINER_NAME:"$CSV_TARGET_DIR/dane_firmowe.csv"
 
-# Tworzenie tabel w MySQL
-echo "Tworzenie tabel w bazie danych..."
-docker exec -i $CONTAINER_NAME mysql -u$DB_ROOT_USER -p$DB_ROOT_PASSWORD <<EOF
-CREATE DATABASE IF NOT EXISTS $DB_NAME;
-USE $DB_NAME;
-
+# Importowanie danych do MySQL z obsługą konfliktów
+echo "Importowanie danych z obsługą konfliktów do tabel MySQL..."
+docker exec -i $CONTAINER_NAME mysql -u$DB_USER -p$DB_PASSWORD -D $DB_NAME <<EOF
+-- Tworzenie tabel, jeśli jeszcze nie istnieją
 CREATE TABLE IF NOT EXISTS dane_osobowe (
-    osoba_id CHAR(50) PRIMARY KEY,
+    osoba_id CHAR(36) PRIMARY KEY,
     imie VARCHAR(50),
     nazwisko VARCHAR(50)
 );
 
 CREATE TABLE IF NOT EXISTS dane_kontaktowe (
-    osoba_id CHAR(50),
+    osoba_id CHAR(36),
     email VARCHAR(100),
     telefon VARCHAR(50),
     ulica VARCHAR(100),
-    numer_domu VARCHAR(30),
+    numer_domu VARCHAR(20),
     miasto VARCHAR(50),
-    kod_pocztowy VARCHAR(50),
+    kod_pocztowy VARCHAR(20),
     FOREIGN KEY (osoba_id) REFERENCES dane_osobowe(osoba_id)
 );
 
 CREATE TABLE IF NOT EXISTS dane_firmowe (
-    osoba_id CHAR(50),
-    nazwa_firmy VARCHAR(150),
-    stanowisko VARCHAR(150),
+    osoba_id CHAR(36),
+    nazwa_firmy VARCHAR(100),
+    stanowisko VARCHAR(100),
     FOREIGN KEY (osoba_id) REFERENCES dane_osobowe(osoba_id)
 );
-EOF
 
-# Import danych z plików CSV do tabel
-echo "Importowanie danych z plików CSV..."
-docker exec -i $CONTAINER_NAME mysql -u$DB_ROOT_USER -p$DB_ROOT_PASSWORD <<EOF
-USE $DB_NAME;
-
+-- Import danych do tabeli dane_osobowe z ignorowaniem duplikatów
 LOAD DATA INFILE '$CSV_TARGET_DIR/dane_osobowe.csv'
-INTO TABLE dane_osobowe
+IGNORE INTO TABLE dane_osobowe
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (osoba_id, imie, nazwisko);
 
+-- Import danych do tabeli dane_kontaktowe z ignorowaniem duplikatów
 LOAD DATA INFILE '$CSV_TARGET_DIR/dane_kontaktowe.csv'
-INTO TABLE dane_kontaktowe
+IGNORE INTO TABLE dane_kontaktowe
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (osoba_id, email, telefon, ulica, numer_domu, miasto, kod_pocztowy);
 
+-- Import danych do tabeli dane_firmowe z nadpisywaniem istniejących rekordów
 LOAD DATA INFILE '$CSV_TARGET_DIR/dane_firmowe.csv'
-INTO TABLE dane_firmowe
+REPLACE INTO TABLE dane_firmowe
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
