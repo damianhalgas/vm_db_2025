b1ce531b50d8ecf1c8aedc1b4d3552a2cbdcc367
damianhalgas <haxv1ct0ri0us@gmail.com>
2025-01-16 15:50:01 +0100

    mssql: db3

--- scripts/import_mssql.sh
+++ scripts/import_mssql.sh
@@ -3,7 +3,7 @@
 CONTAINER_NAME="mssql-container"
 DB_NAME="mydatabase"
 SA_PASSWORD="StrongPassword123!"
-CSV_SOURCE_DIR="/home/administrator/vm_db_2025/csv/utf-8/50K"
+CSV_SOURCE_DIR="/home/administrator/vm_db_2025/csv/utf-8/100K"
 SQL_SCRIPT_DIR="/tmp/sql_scripts"
 
 # Check if CSV files exist
